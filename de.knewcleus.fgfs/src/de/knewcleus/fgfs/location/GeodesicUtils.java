f4b451ccde9c379dcf4539c0e53d453858896c75
Ralf Gerlich <ralf.gerlich@bsse.biz>
2008-03-15 11:00:39 +0100

    Fixed inverse geodesics calculations (handling special cases).

--- de.knewcleus.fgfs/src/de/knewcleus/fgfs/location/GeodesicUtils.java
+++ de.knewcleus.fgfs/src/de/knewcleus/fgfs/location/GeodesicUtils.java
@@ -86,22 +86,22 @@ public class GeodesicUtils {
 		this.ellipsoid=ellipsoid;
 	}
 
-	public GeodesicInformation direct(double startLon, double startLat, double startAzimuth, double length) {
+	public GeodesicInformation direct(double startLongitude, double startLatitude, double startAzimuth, double length) {
 		if (abs(length) < 0.01) {
 			/* Distance lower than 1cm, that's much more detailed than we want it */
 			double az2=startAzimuth+180.0*Units.DEG;
 			if (az2>360.0*Units.DEG) {
 				az2-=360.0*Units.DEG;
 			}
-			return new GeodesicInformation(startLon,startLat,startAzimuth,startLon,startLat,az2,length);
+			return new GeodesicInformation(startLongitude,startLatitude,startAzimuth,startLongitude,startLatitude,az2,length);
 		}
 		
 		final double epsilon=1.0E-10;
 		final double f=ellipsoid.getF();
 		final double b=ellipsoid.getB();
 		final double e2=ellipsoid.getEsquared();
-		final double phi1=startLat/Units.RAD;
-		final double lamda1=startLon/Units.RAD;
+		final double phi1=startLatitude/Units.RAD;
+		final double lambda1=startLongitude/Units.RAD;
 		final double sinphi1=sin(phi1);
 		final double cosphi1=cos(phi1);
 		final double alpha1=startAzimuth/Units.RAD;
@@ -122,16 +122,17 @@ public class GeodesicUtils {
 			final double tb = usq*(256.0+usq*(-128.0+usq*(74.0-47.0*usq)))/1024.0;
 
 			final double firstsigma = length/(b*ta);
-			double sigma = firstsigma;
-			double cossigmam2, sinsigma, cossigma, oldsigma;
+			double dsigma=0;
+			double cossigmam2, sinsigma, cossigma;
+			double sigma;
 
 			do {
+				sigma=firstsigma+dsigma;
 				cossigmam2 = cos(2.0*sigma1+sigma);
 				sinsigma = sin(sigma);
 				cossigma = cos(sigma);
-				oldsigma = sigma;
-				sigma = firstsigma + tb*sinsigma*(cossigmam2+tb*(cossigma*(-1.0+2.0*cossigmam2*cossigmam2) -  tb*cossigmam2*(-3.0+4.0*sinsigma*sinsigma)*(-3.0+4.0*cossigmam2*cossigmam2)/6.0)/4.0);
-			} while ( abs(sigma-oldsigma) > epsilon );
+				dsigma = tb*sinsigma*(cossigmam2+tb*(cossigma*(-1.0+2.0*cossigmam2*cossigmam2) -  tb*cossigmam2*(-3.0+4.0*sinsigma*sinsigma)*(-3.0+4.0*cossigmam2*cossigmam2)/6.0)/4.0);
+			} while ( abs(dsigma) > epsilon );
 			
 			final double temp = sinu1*sinsigma-cosu1*cossigma*cosalpha1;
 			
@@ -146,7 +147,7 @@ public class GeodesicUtils {
 			final double dlambda = atan2(numerdlambda, denomdlambda);
 			final double tc = f*cos2alpha*(4.0+f*(4.0-3.0*cos2alpha))/16.0;
 			final double dl = dlambda - (1.0-tc)*f*sinalpha*(sigma+tc*sinsigma*(cossigmam2+tc*cossigma*(-1.0+2.0*cossigmam2*cossigmam2)));
-			final double lambda2 = lamda1+dl;
+			final double lambda2 = lambda1+dl;
 			
 			/* Calculate endpoint azimuth */
 			final double az2 = atan2(-sinalpha,temp);
@@ -165,34 +166,65 @@ public class GeodesicUtils {
 				endAzimuth+=360.0*Units.DEG;
 			}
 			
-			return new GeodesicInformation(startLon,startLat,startAzimuth,endLongitude,endLatitude,endAzimuth,length);
+			return new GeodesicInformation(startLongitude,startLatitude,startAzimuth,endLongitude,endLatitude,endAzimuth,length);
 		} else {
 			/* Polar origin */
 			final double ds = ellipsoid.getA()*ellipsoid.getM0() - length;
-			final double az = ( startLat < 0 ? 180.0 * Units.DEG : 0.0);
-			return direct(startLon,0,az,ds);
+			final double az = ( startLatitude < 0 ? 180.0 * Units.DEG : 0.0);
+			return direct(startLongitude,0,az,ds);
 		}
 	}
 	
 	public GeodesicInformation inverse(double startLon, double startLat, double endLon, double endLat) {
+		final double epsilon=1.0E-10;
 		final double phi1=startLat/Units.RAD;
 		final double phi2=endLat/Units.RAD;
 		final double lambda1 = startLon/Units.RAD;
 		final double lambda2 = endLon/Units.RAD;
+		
+		if (abs(lambda2-lambda1)<epsilon && abs(phi2-phi1)<epsilon) {
+			/* Start and endpoint are identical */
+			return new GeodesicInformation(startLon,startLat,0,endLon,endLat,0,0);
+		}
+		final double sinphi1=sin(phi1);
+		final double cosphi1=cos(phi1);
+		
+		if (abs(cosphi1)<epsilon) {
+			/* Initial station is polar */
+			GeodesicInformation geodesicInformation=inverse(endLon,endLat,startLon,startLat);
+			return new GeodesicInformation(startLon,startLat,geodesicInformation.endAzimuth,endLon,endLat,geodesicInformation.startAzimuth,geodesicInformation.length);
+		}
+		final double sinphi2=sin(phi2);
+		final double cosphi2=cos(phi2);
+		
+		if (abs(cosphi2)<epsilon) {
+			/* Final point is polar => calculate distance/azimuth to mirrored start point and divide it into two halves */
+			GeodesicInformation geodesicInformation=inverse(startLon, startLat, startLon+180.0*Units.DEG,startLat);
+			double endAzimuth=geodesicInformation.startAzimuth+180.0*Units.DEG;
+			if (endAzimuth>360.0*Units.DEG) {
+				endAzimuth-=360.0*Units.DEG;
+			}
+			return new GeodesicInformation(startLon, startLat, geodesicInformation.startAzimuth, endLon, endLat, endAzimuth, geodesicInformation.length/2);
+		}
+		
+		if (abs(abs(startLon-endLon)-180)<epsilon && abs(startLat+endLat)<epsilon) {
+			/* Geodesic passes through the pole */
+			// FIXME: Not sure about that...
+			GeodesicInformation geodesicInformation1=inverse(startLon,startLat,endLon,startLat);
+			GeodesicInformation geodesicInformation2=inverse(endLon,endLat,endLon,startLat);
+			
+			return new GeodesicInformation(startLon,startLat,geodesicInformation2.startAzimuth,endLon,endLat,geodesicInformation2.startAzimuth,geodesicInformation1.length+geodesicInformation2.length);
+		}
+
 		final double dl = lambda2 - lambda1;
 		
-		final double epsilon=1.0E-10;
 		final double f=ellipsoid.getF();
 		final double b=ellipsoid.getB();
 		final double e2=ellipsoid.getEsquared();
 		
-		final double sinphi1=sin(phi1);
-		final double cosphi1=cos(phi1);
 		final double tanu1 = (1-f)*sinphi1/cosphi1;
 		final double cosu1 = 1.0/sqrt(1.0 + tanu1*tanu1);
 		final double sinu1 = tanu1*cosu1;
-		final double sinphi2=sin(phi2);
-		final double cosphi2=cos(phi2);
 		final double tanu2 = (1-f)*sinphi2/cosphi2;
 		final double cosu2 = 1.0/sqrt(1.0 + tanu2*tanu2);
 		final double sinu2 = tanu2*cosu2;
