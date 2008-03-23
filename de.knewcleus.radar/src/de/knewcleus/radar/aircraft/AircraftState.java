a0f03d3cc319db565162ecde9b3d11049997638d
Ralf Gerlich <ralf.gerlich@bsse.biz>
2008-03-23 23:09:07 +0100

    Implemented basic callsign menu actions.

--- de.knewcleus.radar/src/de/knewcleus/radar/aircraft/AircraftState.java
+++ de.knewcleus.radar/src/de/knewcleus/radar/aircraft/AircraftState.java
@@ -1,13 +1,16 @@
 package de.knewcleus.radar.aircraft;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.Action;
 
 public class AircraftState {
 	protected final String callsign;
-	protected AircraftTaskState taskState=AircraftTaskState.ASSUMED; // FIXME: this should actually be set explicitly
+	protected AircraftTaskState taskState=AircraftTaskState.PENDING_IN;
+	protected final AssumeAction assumeAction=new AssumeAction(this);
+	protected final TransferAction transferAction=new TransferAction(this);
+	protected final SkipAction skipAction=new SkipAction(this);
 	
 	public AircraftState(String callsign) {
 		this.callsign=callsign;
@@ -18,6 +21,35 @@ public class AircraftState {
 	}
 	
 	public List<Action> getAvailableActions() {
-		return Collections.emptyList();
+		List<Action> actionList=new ArrayList<Action>();
+		
+		if (taskState==AircraftTaskState.PENDING) {
+			actionList.add(skipAction);
+		}
+		
+		if (taskState==AircraftTaskState.PENDING_IN) {
+			actionList.add(assumeAction);
+		}
+		
+		if (taskState==AircraftTaskState.ASSUMED) {
+			actionList.add(transferAction);
+		}
+		
+		return actionList;
+	}
+	
+	public void assume() {
+		assert(taskState==AircraftTaskState.PENDING_IN);
+		taskState=AircraftTaskState.ASSUMED;
+	}
+	
+	public void transfer() {
+		assert(taskState==AircraftTaskState.ASSUMED);
+		taskState=AircraftTaskState.ASSUMED_OUT;
+	}
+	
+	public void skip() {
+		assert(taskState==AircraftTaskState.PENDING);
+		taskState=AircraftTaskState.ASSUMED_OUT;
 	}
 }
