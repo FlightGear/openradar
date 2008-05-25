b8bcb7c624c26505a513060a69c0884ed1fbcbd7
Ralf Gerlich <ralf.gerlich@bsse.biz>
2008-05-25 10:22:26 +0200

    Test main() for BuddySquawkAllocator.

---
+++ de.knewcleus.radar.test/src/de/knewcleus/radar/aircraft/test/BuddyTest.java
@@ -0,0 +1,37 @@
+package de.knewcleus.radar.aircraft.test;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
+import de.knewcleus.radar.aircraft.BuddySquawkAllocator;
+import de.knewcleus.radar.aircraft.OutOfSquawksException;
+
+public class BuddyTest {
+	public static void main(String[] args) {
+		BuddySquawkAllocator squawkAllocator=new BuddySquawkAllocator();
+		final Set<String> set=new HashSet<String>();
+		final Deque<String> queue=new ArrayDeque<String>();
+		final Random random=new Random();
+		
+		for (int i=0;i<100000;i++) {
+			if (!queue.isEmpty() && random.nextDouble()<0.5) {
+				final String last=queue.pop();
+				set.remove(last);
+				squawkAllocator.returnSquawk(last);
+			} else {
+				String code;
+				try {
+					code = squawkAllocator.allocateSquawk();
+				} catch (OutOfSquawksException e) {
+					continue;
+				}
+				assert(!set.contains(code));
+				queue.push(code);
+				set.add(code);
+			}
+		}
+	}
+}
