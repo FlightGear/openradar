d189b3ff3ae535d92ebd81d7e532e4e892670b93
Ralf Gerlich <ralf.gerlich@bsse.biz>
2008-06-01 13:27:18 +0200

    Revert "Assume that player callsign is unique, as player address may change."
    This reverts commit c97c07df53f82c7cc9368f783617da9e092fab22.
    
    The change introduced multiple "echoes" of the aircraft, obviously due to the protocol reporting multiple instances of the same aircraft.

--- de.knewcleus.fgfs/src/de/knewcleus/fgfs/multiplayer/AbstractPlayerRegistry.java
+++ de.knewcleus.fgfs/src/de/knewcleus/fgfs/multiplayer/AbstractPlayerRegistry.java
@@ -15,7 +15,7 @@ public abstract class AbstractPlayerRegistry<T extends Player> implements IPlaye
 	public abstract T createNewPlayer(PlayerAddress address, String callsign) throws MultiplayerException;
 
 	protected final Set<T> players = new HashSet<T>();
-	protected final Map<String, T> playersByAddress = new HashMap<String, T>();
+	protected final Map<PlayerAddress, T> playersByAddress = new HashMap<PlayerAddress, T>();
 
 	public AbstractPlayerRegistry() {
 		super();
@@ -34,12 +34,12 @@ public abstract class AbstractPlayerRegistry<T extends Player> implements IPlaye
 		if (hasPlayer(player.getAddress()))
 			unregisterPlayer(getPlayer(player.getAddress()));
 		players.add(player);
-		playersByAddress.put(player.getCallsign(),player);
+		playersByAddress.put(player.getAddress(),player);
 	}
 
 	public synchronized void unregisterPlayer(T expiredPlayer) {
 		players.remove(expiredPlayer);
-		playersByAddress.remove(expiredPlayer.getCallsign());
+		playersByAddress.remove(expiredPlayer.getAddress());
 	}
 	
 	public Collection<T> getPlayers() {
