28372125d8ae68c04b09ab1cf3452a70474e03d4
Ralf Gerlich <ralf.gerlich@bsse.biz>
2008-06-05 20:41:21 +0200

    Finally implemented getBounds()-method for TransformedShape.

--- de.knewcleus.openradar/src/de/knewcleus/openradar/utils/TransformedShape.java
+++ de.knewcleus.openradar/src/de/knewcleus/openradar/utils/TransformedShape.java
@@ -31,12 +31,12 @@ public class TransformedShape implements Shape {
 
 	@Override
 	public boolean contains(double x, double y) {
-		return Path2D.contains(getPathIterator(new AffineTransform()), x, y);
+		return Path2D.contains(getPathIterator(null), x, y);
 	}
 
 	@Override
 	public boolean contains(double x, double y, double w, double h) {
-		return Path2D.contains(getPathIterator(new AffineTransform()), x, y, w, h);
+		return Path2D.contains(getPathIterator(null), x, y, w, h);
 	}
 
 	@Override
@@ -46,17 +46,17 @@ public class TransformedShape implements Shape {
 
 	@Override
 	public boolean intersects(double x, double y, double w, double h) {
-		return Path2D.intersects(getPathIterator(new AffineTransform()), x, y, w, h);
+		return Path2D.intersects(getPathIterator(null), x, y, w, h);
 	}
 
 	@Override
 	public PathIterator getPathIterator(AffineTransform at) {
-		return new TransformingPathIterator(at,originalShape.getPathIterator(new AffineTransform()),deviceTransformation);
+		return new TransformingPathIterator(at,originalShape.getPathIterator(null),deviceTransformation);
 	}
 
 	@Override
 	public PathIterator getPathIterator(AffineTransform at, double flatness) {
-		return new TransformingPathIterator(at,originalShape.getPathIterator(new AffineTransform(), flatness),deviceTransformation);
+		return new TransformingPathIterator(at,originalShape.getPathIterator(null, flatness),deviceTransformation);
 	}
 
 	@Override
@@ -66,7 +66,23 @@ public class TransformedShape implements Shape {
 
 	@Override
 	public Rectangle2D getBounds2D() {
-		// TODO Auto-generated method stub
-		return null;
+		final PathIterator pi=getPathIterator(null);
+		final double coords[]=new double[6];
+		Rectangle2D bounds=new Rectangle2D.Double();
+		while(!pi.isDone()) {
+			final int type=pi.currentSegment(coords);
+			switch (type) {
+			case PathIterator.SEG_QUADTO:
+				bounds.add(coords[4], coords[5]);
+			case PathIterator.SEG_CUBICTO:
+				bounds.add(coords[2], coords[3]);
+			case PathIterator.SEG_MOVETO:
+			case PathIterator.SEG_LINETO:
+				bounds.add(coords[0], coords[1]);
+			case PathIterator.SEG_CLOSE:
+				break;
+			}
+		}
+		return bounds;
 	}
 }
