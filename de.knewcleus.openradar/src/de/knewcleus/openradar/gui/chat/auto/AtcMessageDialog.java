40d09837cb6f25043793751b05505319777db8ef
wwagner <wolfram@wagnerw.de>
2013-03-29 09:58:07 +0100

    - improved chat dialog - HTML support for chat selection (cmd.short.txt)

--- de.knewcleus.openradar/src/de/knewcleus/openradar/gui/chat/auto/AtcMessageDialog.java
+++ de.knewcleus.openradar/src/de/knewcleus/openradar/gui/chat/auto/AtcMessageDialog.java
@@ -1,32 +1,32 @@
 /**
- * Copyright (C) 2012,2013 Wolfram Wagner 
- * 
+ * Copyright (C) 2012,2013 Wolfram Wagner
+ *
  * This file is part of OpenRadar.
- * 
+ *
  * OpenRadar is free software: you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
  * Foundation, either version 3 of the License, or (at your option) any later
  * version.
- * 
+ *
  * OpenRadar is distributed in the hope that it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
  * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along with
  * OpenRadar. If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  * Diese Datei ist Teil von OpenRadar.
- * 
+ *
  * OpenRadar ist Freie Software: Sie können es unter den Bedingungen der GNU
  * General Public License, wie von der Free Software Foundation, Version 3 der
  * Lizenz oder (nach Ihrer Option) jeder späteren veröffentlichten Version,
  * weiterverbreiten und/oder modifizieren.
- * 
+ *
  * OpenRadar wird in der Hoffnung, dass es nützlich sein wird, aber OHNE JEDE
  * GEWÄHELEISTUNG, bereitgestellt; sogar ohne die implizite Gewährleistung der
  * MARKTFÄHIGKEIT oder EIGNUNG FÜR EINEN BESTIMMTEN ZWECK. Siehe die GNU General
  * Public License für weitere Details.
- * 
+ *
  * Sie sollten eine Kopie der GNU General Public License zusammen mit diesem
  * Programm erhalten haben. Wenn nicht, siehe <http://www.gnu.org/licenses/>.
  */
@@ -48,9 +48,9 @@ import java.awt.geom.Point2D;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JSeparator;
 
 import de.knewcleus.openradar.gui.GuiMasterController;
 import de.knewcleus.openradar.gui.contacts.GuiRadarContact;
@@ -60,11 +60,11 @@ public class AtcMessageDialog extends JFrame {
     private static final long serialVersionUID = 1L;
     private final GuiMasterController master;
     private final TextManager manager;
-    private final JList<AtcMessage> liMessages = new JList<AtcMessage>();
+    private final JPanel pnlMessages = new JPanel();
     private JScrollPane spList;
 
     private JLabel lbCallSign = new JLabel();
-    
+
     public AtcMessageDialog(GuiMasterController master, TextManager manager) {
         this.master = master;
         this.manager = manager;
@@ -84,7 +84,7 @@ public class AtcMessageDialog extends JFrame {
             this.setOpacity(0.8f);
         }
 
-        
+
         JPanel jPnlContentPane = new JPanel();
         jPnlContentPane.setOpaque(false);
         jPnlContentPane.setLayout(new GridBagLayout());
@@ -110,15 +110,31 @@ public class AtcMessageDialog extends JFrame {
         gridBagConstraints.insets = new java.awt.Insets(4, 4, 4, 2);
         jPnlContentPane.add(spList, gridBagConstraints);
 
-        spList.setViewportView(liMessages);
-        
-        liMessages.setModel(manager);
-        liMessages.setSelectionForeground(liMessages.getForeground());
-        liMessages.setSelectionBackground(liMessages.getBackground());
-        liMessages.addMouseListener(new TextMouseListener());
-        liMessages.setOpaque(false);
-        liMessages.setSelectionBackground(liMessages.getBackground());
-        
+        spList.setViewportView(pnlMessages);
+
+        pnlMessages.setLayout(new GridBagLayout());
+
+        for(int i=0;i<manager.getSize();i++) {
+            gridBagConstraints = new java.awt.GridBagConstraints();
+            gridBagConstraints.gridx = 0;
+            gridBagConstraints.gridy = i;
+            gridBagConstraints.anchor = GridBagConstraints.WEST;
+            gridBagConstraints.insets = new java.awt.Insets(2, 4, 2, 2);
+
+            String msg = manager.getElementAt(i).getDisplayMessage();
+            if(msg.isEmpty()) {
+                gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
+                gridBagConstraints.weightx = 1.0;
+                pnlMessages.add(new JSeparator(), gridBagConstraints);
+            } else {
+                JLabel lb = new JLabel("<html>"+msg+"</html>");
+                lb.setName(""+i);
+                lb.addMouseListener(new TextMouseListener());
+                pnlMessages.add(lb, gridBagConstraints);
+            }
+        }
+        pnlMessages.setOpaque(false);
+
         doLayout();
     }
 
@@ -128,11 +144,11 @@ public class AtcMessageDialog extends JFrame {
 
         GraphicsEnvironment env = GraphicsEnvironment.getLocalGraphicsEnvironment();
         Rectangle maxBounds = env.getMaximumWindowBounds();
-        
+
         // size scrollpane
-        Dimension preferredSize = liMessages.getPreferredSize();
-        spList.setPreferredSize(preferredSize.getHeight()>maxBounds.getHeight() ? 
-                new Dimension(liMessages.getPreferredSize().width+30, maxBounds.height-40) : liMessages.getPreferredSize());
+        Dimension preferredSize = pnlMessages.getPreferredSize();
+        spList.setPreferredSize(preferredSize.getHeight()>maxBounds.getHeight() ?
+                new Dimension(pnlMessages.getPreferredSize().width+30, maxBounds.height-40) : pnlMessages.getPreferredSize());
 
         // side dialog
         Dimension innerSize = getContentPane().getPreferredSize();
@@ -141,7 +157,7 @@ public class AtcMessageDialog extends JFrame {
         Point2D p = /*e.getSource() instanceof JList ?
                 ((JComponent) e.getSource()).getLocationOnScreen():*/
                 e.getLocationOnScreen();
-        p = new Point2D.Double(p.getX() - this.getWidth() - 10, p.getY());
+        p = new Point2D.Double(p.getX() - 100 , p.getY()); //- this.getWidth() - 10
 
         int lowerDistanceToScreenBorder=50;
         if(p.getY()+getHeight()>maxBounds.getHeight()-lowerDistanceToScreenBorder) {
@@ -150,18 +166,18 @@ public class AtcMessageDialog extends JFrame {
         setLocation(new Point((int) p.getX(), (int) p.getY()));
         setVisible(true);
     }
-    
+
     private class TextMouseListener extends MouseAdapter {
         @Override
         public void mouseClicked(MouseEvent e) {
-            AtcMessage msg = manager.getElementAt(liMessages.locationToIndex(e.getPoint()));
+            AtcMessage msg = manager.getElementAt(Integer.parseInt(((JLabel)e.getSource()).getName()));
             if(msg!=null) {
                 closeDialog();
                 master.getMpChatManager().setAutoAtcMessage(msg);
             }
         }
     }
-    
+
     private class DialogCloseListener extends WindowAdapter {
         @Override
         public void windowClosed(WindowEvent e) {
@@ -183,5 +199,5 @@ public class AtcMessageDialog extends JFrame {
     private void closeDialog() {
         setVisible(false);
     }
-    
+
 }
